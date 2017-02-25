669f7c90132d2985d4114c92bc0bc207ce9223ff
Vasya Novikov <n1dr+cm3513git@ya.ru>
2017-02-25 19:55:25 +0300

    my friends are very accurate

--- color_smart.glsl
+++ color_smart.glsl
@@ -10,7 +10,7 @@ uniform sampler2D tex;
  *
  * In contrast to color matrix multiplication techniques,
  * it keeps the colors "rich" (they take full RGB spectrum)
- * and it precisely retains "hue" component of the color. 
+ * and it precisely retains "hue" component of the color.
  * (Matrices don't do that.)
  *
  * See screenshots if in doubt.
@@ -31,14 +31,14 @@ void main() {
 		// "new_color" the new color we want to derive,
 		// "old_c" the old color component and
 		// "new_c" the new color component.
-		// 
+		//
 		// We have the following equasion then:
 		// new_color = rgb_negation(hue_180_rotate(old_color)) <=>
 		// new_c = rgb_negation(reflection_around_min_max_rgb(old_c)) <=>
 		// new_c = rgb_negation(max(r,g,b) - (old_c - min(r,g,b))) <=>
 		// new_c = old_alpha - (max(r,g,b) - old_c + min(r,g,b)) <=>
 		// new_c = old_alpha - max(r,g,b) - min (r,g,b) + old_c
-	
+
 		float total = c.a - min(c.r, min(c.g, c.b)) - max(c.r, max(c.g, c.b));
 		c = vec4(total + c.r, total + c.g, total + c.b, c.a);
 	}
