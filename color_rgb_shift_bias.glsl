33d3e4c3da20803c15254629ffef4c8a0aa6d7d8
Vasya Novikov <n1dr+cm3513git@ya.ru>
2017-03-10 12:23:04 +0300

    fix alpha multiplier

--- color_rgb_shift_bias.glsl
+++ color_rgb_shift_bias.glsl
@@ -7,7 +7,7 @@ void main() {
 	if (invert_color) {
 		float bias = 1.5; // The RGB step 254->255 is "bias" more important than 0->1
 		float axis_shift = 1.0 / (bias - 1.0);
-		float axis_inversion_length = (axis_shift + 1.0)*(axis_shift + 1.0) + axis_shift*axis_shift;
+		float axis_inversion_length = c.a*(axis_shift + 1.0)*(axis_shift + 1.0) + axis_shift*axis_shift;
 		c = vec4((c.r+axis_shift)*(c.r+axis_shift), (c.g+axis_shift)*(c.g+axis_shift), (c.b+axis_shift)*(c.b+axis_shift), c.a);
 		float shift = axis_inversion_length - min(c.r, min(c.g, c.b)) - max(c.r, max(c.g, c.b));
 		c = vec4(shift + c.r, shift + c.g, shift + c.b, c.a);
