779f4237cfbeef57cb9ad4f42e5540d8626486c5
Vasya Novikov <n1dr+cm3513git@ya.ru>
2018-09-28 15:58:10 +0300

    clean up negate_whiteish.glsl

--- negate_whiteish.glsl
+++ negate_whiteish.glsl
@@ -3,11 +3,10 @@ uniform float opacity;
 uniform sampler2D tex;
 
 void main() {
-	float white_bias = 0.13;
+	float max = 1.13;
 
 	vec4 c = texture2D(tex, gl_TexCoord[0].st);
 	if (invert_color) {
-		float max = white_bias + 1.0;
 		c = vec4((max - c.r) / max, (max - c.g) / max, (max - c.b) / max, c.a);
 	}
 	c *= opacity;
